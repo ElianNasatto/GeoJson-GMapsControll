8bf56dab7d0d34e55588f0e90139a441d466073d
Elian Nasatto <elian.nasa123@gmail.com>
2020-02-11 10:09:14 -0300

    Maps e json

---
+++ WindowsFormsApp3/WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gMapControl1 = new GMap.NET.WindowsForms.GMapControl();
+            this.SuspendLayout();
+            // 
+            // gMapControl1
+            // 
+            this.gMapControl1.AllowDrop = true;
+            this.gMapControl1.Bearing = 0F;
+            this.gMapControl1.CanDragMap = true;
+            this.gMapControl1.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gMapControl1.GrayScaleMode = false;
+            this.gMapControl1.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gMapControl1.LevelsKeepInMemory = 5;
+            this.gMapControl1.Location = new System.Drawing.Point(43, 126);
+            this.gMapControl1.MarkersEnabled = true;
+            this.gMapControl1.MaxZoom = 18;
+            this.gMapControl1.MinZoom = 2;
+            this.gMapControl1.MouseWheelZoomEnabled = true;
+            this.gMapControl1.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gMapControl1.Name = "gMapControl1";
+            this.gMapControl1.NegativeMode = false;
+            this.gMapControl1.PolygonsEnabled = true;
+            this.gMapControl1.RetryLoadTile = 0;
+            this.gMapControl1.RoutesEnabled = true;
+            this.gMapControl1.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gMapControl1.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gMapControl1.ShowTileGridLines = false;
+            this.gMapControl1.Size = new System.Drawing.Size(688, 297);
+            this.gMapControl1.TabIndex = 0;
+            this.gMapControl1.Zoom = 15D;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gMapControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GMap.NET.WindowsForms.GMapControl gMapControl1;
+    }
+}
+
---
+++ WindowsFormsApp3/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,103 @@
+﻿using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            gMapControl1.MapProvider = GMap.NET.MapProviders.GoogleMapProvider.Instance;
+            GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerOnly;
+            //gMapControl1.SetPositionByKeywords("Paris, France");
+            gMapControl1.Position = new PointLatLng(48.866383, 2.323575);
+
+            //PointLatLng result = new PointLatLng();
+            //gMapControl1.GetPositionByKeywords("US",out result);
+            WebClient client = new WebClient();
+            //Download de um arquivo JSON
+            string geoJson = client.DownloadString("http://mapas.ammvi.org.br/geoserver/wfs?srsName=EPSG%3A4326&typename=ammvi%3Abairros&outputFormat=json&version=1.0.0&service=WFS&request=GetFeature");
+            //var pontos = JsonConvert.SerializeObject(casa);
+            //var oi = new GeoJSON.Net.Converters.GeoJsonConverter().ReadJson;
+            var oi = JsonConvert.DeserializeObject(geoJson);
+
+            GMapOverlay polygons = new GMapOverlay("polygons");
+            List<PointLatLng> points = new List<PointLatLng>();
+            points.Add(new PointLatLng(48.866383, 2.323575));
+            points.Add(new PointLatLng(48.863868, 2.321554));
+            points.Add(new PointLatLng(48.861017, 2.330030));
+            points.Add(new PointLatLng(48.863727, 2.331918));
+            GMapPolygon polygon = new GMapPolygon(points, "Jardin des Tuileries");
+            polygon.Fill = new SolidBrush(Color.FromArgb(50, Color.Red));
+            polygon.Stroke = new Pen(Color.Red, 1);
+            polygons.Polygons.Add(polygon);
+            gMapControl1.Overlays.Add(polygons);
+
+        }
+    }
+
+    public class Geison
+    {
+        string type { get; set; }
+        string properties { get; set; }
+        Features features { get; set; }
+    }
+
+    public class Features
+    {
+        FeatureGeometry feature { get; set; }
+
+    }
+    public class FeatureExtra
+    {
+        string type { get; set; }
+        string id { get; set; }
+
+        string geometry { get; set; }
+
+    }
+    public class FeatureGeometry
+    {
+        string type { get; set; }
+
+        string id { get; set; }
+
+    }
+
+    public class Geometry
+    {
+        string type { get; set; }
+
+
+        string[] cordinates { get; set; }
+
+        string geometry_name { get; set; }
+
+        PropertiesGeometry properties { get; set; }
+
+    }
+
+    public class PropertiesGeometry
+    {
+        string Nome { get; set; }
+    }
+
+}
+
