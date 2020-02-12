62aa79adf58e1740876c3bea6d063cef6549c0c6
Elian Nasatto <elian.nasa123@gmail.com>
2020-02-12 13:40:45 -0300

    Correções para converter o geoJson em objeto

--- WindowsFormsApp3/WindowsFormsApp3/Form1.cs
+++ WindowsFormsApp3/WindowsFormsApp3/Form1.cs
@@ -1,4 +1,7 @@
-﻿using GMap.NET;
+﻿using GeoJSON.Net;
+using GeoJSON.Net.Converters;
+using GeoJSON.Net.Geometry;
+using GMap.NET;
 using GMap.NET.MapProviders;
 using GMap.NET.WindowsForms;
 using GMap.NET.WindowsForms.Markers;
@@ -15,10 +18,61 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using GeoJsonSharp;
 
 namespace WindowsFormsApp3
 {
+    public class FeatureCollection
+    {
+        public string Type { get; set; }
+        public string TotalFeatures { get; set; }
+        public List<Features> Features { get; set; }
 
+        public CRS Crs { get; set; }
+    }
+
+    public class CRS
+    {
+        public string Type { get; set; }
+        public Proprieties2 Proprieties { get; set; }
+    }
+    public class Proprieties2
+    {
+        public string Name { get; set; }
+    }
+    public class Features
+    {
+        public string Type { get; set; }
+        public string Id { get; set; }
+        public Geometry Geometry { get; set; }
+    }
+
+    public class Geometry
+    {
+        public string Type { get; set; }
+        public List<MultiPolygon> MultiPolygon { get; set; }
+        public string GeometryName { get; set; }
+        public Proprieties Proprieties { get; set; }
+    }
+
+    public class Proprieties
+    {
+        public string Nome { get; set; }
+    }
+
+    public class MultiPolygon
+    {
+        public string Type { get; set; }
+        public string[][] Coordinates { get; set; }
+
+        /* lista [
+         *          lista[
+         *          [[
+         *          ]]]
+         *          ]
+         *          
+         *          */
+    }
     public partial class Form1 : Form
     {
         public Form1()
@@ -29,14 +83,24 @@ namespace WindowsFormsApp3
             //gMapControl1.SetPositionByKeywords("Paris, France");
             gMapControl1.Position = new PointLatLng(48.866383, 2.323575);
 
-            //PointLatLng result = new PointLatLng();
-            //gMapControl1.GetPositionByKeywords("US",out result);
+            ////PointLatLng result = new PointLatLng();
+            ////gMapControl1.GetPositionByKeywords("US",out result);
             WebClient client = new WebClient();
-            //Download de um arquivo JSON
+            ////Download de um arquivo JSON
             string geoJson = client.DownloadString("http://mapas.ammvi.org.br/geoserver/wfs?srsName=EPSG%3A4326&typename=ammvi%3Abairros&outputFormat=json&version=1.0.0&service=WFS&request=GetFeature");
-            //var pontos = JsonConvert.SerializeObject(casa);
-            //var oi = new GeoJSON.Net.Converters.GeoJsonConverter().ReadJson;
-            var oi = JsonConvert.DeserializeObject(geoJson);
+            //geoJson = geoJson.Replace("[[", "").Replace("]]", "");
+            ////var pontos = JsonConvert.SerializeObject(casa);
+            ////var oi = new GeoJSON.Net.Converters.GeoJsonConverter().ReadJson;
+            var oi = JsonConvert.DeserializeObject<FeatureCollection>(geoJson);
+            ////string json = "{\"coordinates\":[-2.124156,51.899523],\"type\":\"Point\"}";
+
+            ////var olamundo = GeoJsonConverter.DeserializeObject(geoJson);
+            //var hehe = new GeoJsonParser(geoJson,new ParserSettings());
+            //GMapPolygon point = JsonConvert.DeserializeObject<GMapPolygon>(geoJson);
+            string valores = client.DownloadString("http://mapas.ammvi.org.br/geoserver/wfs?typename=ammvi%3Abairros&outputFormat=text%2Fxml%3B+subtype%3Dgml%2F3.1.1&version=1.0.0&request=GetFeature&service=WFS");
+
+
+
 
             GMapOverlay polygons = new GMapOverlay("polygons");
             List<PointLatLng> points = new List<PointLatLng>();
@@ -51,52 +115,6 @@ namespace WindowsFormsApp3
             gMapControl1.Overlays.Add(polygons);
 
         }
-    }
-
-    public class Geison
-    {
-        string type { get; set; }
-        string properties { get; set; }
-        Features features { get; set; }
-    }
-
-    public class Features
-    {
-        FeatureGeometry feature { get; set; }
-
-    }
-    public class FeatureExtra
-    {
-        string type { get; set; }
-        string id { get; set; }
-
-        string geometry { get; set; }
-
-    }
-    public class FeatureGeometry
-    {
-        string type { get; set; }
-
-        string id { get; set; }
-
-    }
-
-    public class Geometry
-    {
-        string type { get; set; }
-
-
-        string[] cordinates { get; set; }
-
-        string geometry_name { get; set; }
-
-        PropertiesGeometry properties { get; set; }
-
-    }
-
-    public class PropertiesGeometry
-    {
-        string Nome { get; set; }
     }
 
 }
