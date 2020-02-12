63b08711ebcfb66c5b1f05e925e675a7542d4960
Elian Nasatto <elian.nasa123@gmail.com>
2020-02-12 15:03:49 -0300

    Leitura do GeoJson

--- WindowsFormsApp3/WindowsFormsApp3/Form1.Designer.cs
+++ WindowsFormsApp3/WindowsFormsApp3/Form1.Designer.cs
@@ -34,13 +34,14 @@
             // gMapControl1
             // 
             this.gMapControl1.AllowDrop = true;
+            this.gMapControl1.AutoSize = true;
             this.gMapControl1.Bearing = 0F;
             this.gMapControl1.CanDragMap = true;
             this.gMapControl1.EmptyTileColor = System.Drawing.Color.Navy;
             this.gMapControl1.GrayScaleMode = false;
             this.gMapControl1.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
             this.gMapControl1.LevelsKeepInMemory = 5;
-            this.gMapControl1.Location = new System.Drawing.Point(43, 126);
+            this.gMapControl1.Location = new System.Drawing.Point(12, 12);
             this.gMapControl1.MarkersEnabled = true;
             this.gMapControl1.MaxZoom = 18;
             this.gMapControl1.MinZoom = 2;
@@ -54,19 +55,20 @@
             this.gMapControl1.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
             this.gMapControl1.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
             this.gMapControl1.ShowTileGridLines = false;
-            this.gMapControl1.Size = new System.Drawing.Size(688, 297);
+            this.gMapControl1.Size = new System.Drawing.Size(1080, 568);
             this.gMapControl1.TabIndex = 0;
-            this.gMapControl1.Zoom = 15D;
+            this.gMapControl1.Zoom = 12D;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1113, 600);
             this.Controls.Add(this.gMapControl1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Mapa Bairros de Blumenau";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
--- WindowsFormsApp3/WindowsFormsApp3/Form1.cs
+++ WindowsFormsApp3/WindowsFormsApp3/Form1.cs
@@ -1,10 +1,9 @@
-﻿using GeoJSON.Net;
-using GeoJSON.Net.Converters;
-using GeoJSON.Net.Geometry;
-using GMap.NET;
+﻿using GMap.NET;
 using GMap.NET.MapProviders;
 using GMap.NET.WindowsForms;
 using GMap.NET.WindowsForms.Markers;
+using NetTopologySuite.Features;
+using NetTopologySuite.IO;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -18,101 +17,113 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using GeoJsonSharp;
 
 namespace WindowsFormsApp3
 {
-    public class FeatureCollection
-    {
-        public string Type { get; set; }
-        public string TotalFeatures { get; set; }
-        public List<Features> Features { get; set; }
+    //public class FeatureCollection
+    //{
+    //    public string Type { get; set; }
+    //    public string TotalFeatures { get; set; }
+    //    public List<Features> Features { get; set; }
 
-        public CRS Crs { get; set; }
-    }
+    //    public CRS Crs { get; set; }
+    //}
 
-    public class CRS
-    {
-        public string Type { get; set; }
-        public Proprieties2 Proprieties { get; set; }
-    }
-    public class Proprieties2
-    {
-        public string Name { get; set; }
-    }
-    public class Features
-    {
-        public string Type { get; set; }
-        public string Id { get; set; }
-        public Geometry Geometry { get; set; }
-    }
+    //public class CRS
+    //{
+    //    public string Type { get; set; }
+    //    public Proprieties2 Proprieties { get; set; }
+    //}
+    //public class Proprieties2
+    //{
+    //    public string Name { get; set; }
+    //}
+    //public class Features
+    //{
+    //    public string Type { get; set; }
+    //    public string Id { get; set; }
+    //    public Geometry Geometry { get; set; }
+    //}
 
-    public class Geometry
-    {
-        public string Type { get; set; }
-        public List<MultiPolygon> MultiPolygon { get; set; }
-        public string GeometryName { get; set; }
-        public Proprieties Proprieties { get; set; }
-    }
+    //public class Geometry
+    //{
+    //    public string Type { get; set; }
+    //    public List<MultiPolygon> MultiPolygon { get; set; }
+    //    public string GeometryName { get; set; }
+    //    public Proprieties Proprieties { get; set; }
+    //}
 
-    public class Proprieties
-    {
-        public string Nome { get; set; }
-    }
+    //public class Proprieties
+    //{
+    //    public string Nome { get; set; }
+    //}
 
-    public class MultiPolygon
-    {
-        public string Type { get; set; }
-        public string[][] Coordinates { get; set; }
+    //public class MultiPolygon
+    //{
+    //    public string Type { get; set; }
+    //    public string[][] Coordinates { get; set; }
 
-        /* lista [
-         *          lista[
-         *          [[
-         *          ]]]
-         *          ]
-         *          
-         *          */
-    }
+    //    /* lista [
+    //     *          lista[
+    //     *          [[
+    //     *          ]]]
+    //     *          ]
+    //     *          
+    //     *          */
+    //}
+
     public partial class Form1 : Form
     {
+        public System.Drawing.Color RetornaCorAleatoria()
+        {
+            Random random = new Random();
+            int r = random.Next(0,255);
+            int g = random.Next(0,255);
+            int b = random.Next(0,255);
+            //int a = random.Next(0,255);
+            return Color.FromArgb(50,r, g, b);
+        }
         public Form1()
         {
             InitializeComponent();
             gMapControl1.MapProvider = GMap.NET.MapProviders.GoogleMapProvider.Instance;
             GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerOnly;
-            //gMapControl1.SetPositionByKeywords("Paris, France");
-            gMapControl1.Position = new PointLatLng(48.866383, 2.323575);
 
-            ////PointLatLng result = new PointLatLng();
-            ////gMapControl1.GetPositionByKeywords("US",out result);
             WebClient client = new WebClient();
             ////Download de um arquivo JSON
             string geoJson = client.DownloadString("http://mapas.ammvi.org.br/geoserver/wfs?srsName=EPSG%3A4326&typename=ammvi%3Abairros&outputFormat=json&version=1.0.0&service=WFS&request=GetFeature");
-            //geoJson = geoJson.Replace("[[", "").Replace("]]", "");
-            ////var pontos = JsonConvert.SerializeObject(casa);
-            ////var oi = new GeoJSON.Net.Converters.GeoJsonConverter().ReadJson;
-            var oi = JsonConvert.DeserializeObject<FeatureCollection>(geoJson);
-            ////string json = "{\"coordinates\":[-2.124156,51.899523],\"type\":\"Point\"}";
 
-            ////var olamundo = GeoJsonConverter.DeserializeObject(geoJson);
-            //var hehe = new GeoJsonParser(geoJson,new ParserSettings());
-            //GMapPolygon point = JsonConvert.DeserializeObject<GMapPolygon>(geoJson);
-            string valores = client.DownloadString("http://mapas.ammvi.org.br/geoserver/wfs?typename=ammvi%3Abairros&outputFormat=text%2Fxml%3B+subtype%3Dgml%2F3.1.1&version=1.0.0&request=GetFeature&service=WFS");
+            GeoJsonReader reader = new GeoJsonReader();
+            FeatureCollection result = reader.Read<FeatureCollection>(geoJson);
 
+            foreach (var bairro in result)
+            {
+                var coordernadas = bairro.Geometry.Coordinates;
+                GMapOverlay polygons = new GMapOverlay("polygons");
+                List<PointLatLng> points = new List<PointLatLng>();
 
 
+                foreach (var coordernada in coordernadas)
+                {
+                    points.Add(new PointLatLng(coordernada.Y, coordernada.X));
+                }
 
-            GMapOverlay polygons = new GMapOverlay("polygons");
-            List<PointLatLng> points = new List<PointLatLng>();
-            points.Add(new PointLatLng(48.866383, 2.323575));
-            points.Add(new PointLatLng(48.863868, 2.321554));
-            points.Add(new PointLatLng(48.861017, 2.330030));
-            points.Add(new PointLatLng(48.863727, 2.331918));
-            GMapPolygon polygon = new GMapPolygon(points, "Jardin des Tuileries");
-            polygon.Fill = new SolidBrush(Color.FromArgb(50, Color.Red));
-            polygon.Stroke = new Pen(Color.Red, 1);
-            polygons.Polygons.Add(polygon);
-            gMapControl1.Overlays.Add(polygons);
+                GMapPolygon polygon = new GMapPolygon(points, bairro.Attributes["Nome"].ToString());
+                polygon.Fill = new SolidBrush(RetornaCorAleatoria());
+                polygon.Stroke = new Pen(Color.Red, 1);
+                polygons.Polygons.Add(polygon);
+                polygon.Name = bairro.Attributes["Nome"].ToString();
+
+                if (bairro.Attributes["Nome"].ToString() == "CENTRO")
+                {
+                    gMapControl1.Position = new PointLatLng(polygon.Points[0].Lat,polygon.Points[0].Lng);
+                    polygon.Fill = new SolidBrush(Color.FromArgb(50,100,0,0));
+                }
+                gMapControl1.Overlays.Add(polygons);
+
+            }
+
+
 
         }
     }
